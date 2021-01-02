8fe4d4602b8302302880d8639f6ee40e629baa42
Eduard <eduardegomez@gmail.com>
2021-01-02 15:40:14 +0100

    Cambio de patrón Router/Dealer -> Router/Router, archivos Queue.js y Worker.js

--- Worker/Worker.js
+++ Worker/Worker.js
@@ -1,11 +1,14 @@
 const zmq = require('zeromq');
-let rep = zmq.socket('rep');
-rep.connect('tcp://localhost:9999');
+let req = zmq.socket('req');
+let id = req.identity='Worker1' +process.pid
+req.connect('tcp://localhost:9999');
 
 console.log('Im listening now...');
-rep.on('message', (msg)=> {
+console.log('Im number: ' + id);
+req.on('message', (c,sep,msg)=> {
 	console.log('A new message has arrived: ' + msg.toString());
 	setTimeout(()=> {
-		rep.send(msg);
+		req.send([c,'', 'resp'])
 	}, 1000);
 })
+req.send(['','',''])
