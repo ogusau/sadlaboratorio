cb73109e44a9d8b273ab18382a6788e8b2847abe
Eduard <eduardegomez@gmail.com>
2021-01-30 11:22:50 +0100

    Componente frontend modificado para su nueva conexión con Consul. Se añade una función latido para detectar las colas registradas en el servi registry de consul y hacer los connects a todas las colas disponibles, pasados 8 segundos se vuelve a comprobar si se han añadido más colas al sistema para así poder conectarse a ellas.

--- FrontEnd/service/server/service.js
+++ FrontEnd/service/server/service.js
@@ -2,32 +2,60 @@ const express = require('express');
 const zmq = require('zeromq');
 
 const service = express();
-let req_socket = zmq.socket('req');
+let dealer = zmq.socket('dealer');
+const consul = require('consul')({host: '172.16.238.2', port:8500});
+let PORT_Q = '9999';
+const queues = new Set();
+let primera = true;
+
+let isDone = true;
+let resp = '';
 
 module.exports = (config) => {
   const log = config.log();
   const SERVICE_ID = config.serviceID;
   
-  req_socket.connect('tcp://localhost:9998');
-  log.debug('Connected to zmq');
-  req_socket.on('message', (msg)=> {
-	console.log('resp: '+msg)
-  });
+  function sleep(ms){
+  	return new Promise(resolve => setTimeout(resolve, ms));
+  }
   
+  function heartBeats(){
+	consul.agent.service.list(function(err, result){
+		let hasElements = 0;
+		if (err) throw err;
+		for(let i in result)
+		{
+	  		if(result[i].Service.startsWith('Queue')){
+	  			let lastSize = queues.size;
+	  			queues.add(result[i].Address);
+	  			if(queues.size > lastSize){
+	  				let ip = result[i].Address;
+					dealer.connect('tcp://' + ip + ':' + PORT_Q);
+					hasElements++;
+	  			}
+			}
+		}
+		if(hasElements > 0 && primera){
+			primera = false;
+		}
+	});
+  };
+  
+  setInterval(heartBeats, 8000);
+  heartBeats();
+  
+  dealer.on('message', (sep, idusu, sep2, msg) => {
+	log.debug('Respuesta recibida: ' + msg);
+	resp = msg;
+  	isDone = false;
+  });
+
   service.use(express.urlencoded({
 		extended: true
 	}));
   
-  // Add a request logging middleware in development mode
- /* if (service.get('env') === 'development') {
-    service.use((req, res, next) => {
-      log.debug(`${req.method}: ${req.url}`);
-      return next();
-    });
-  }
-  */
-  service.post('/frontEnd/integraPI', (req, res, next) => {
-	
+  service.post('/frontEnd/integraPI', async (req, res, next) => {
+	isDone = true;
 	if(!req.body.inf || !req.body.sup || !req.body.ite) {
 		respuesta = {
 			error: true,
@@ -36,18 +64,17 @@ module.exports = (config) => {
 		};
 		return res.json(respuesta);
 	}
-	mensaje = {
-		uri: '/integraPI',
-		method: 'POST',
+	let mensaje = {
 		inf: req.body.inf,
 		sup: req.body.sup,
 		ite: req.body.ite
 	};
 	log.debug('Sending message: ' + JSON.stringify(mensaje));
-	req_socket.send(JSON.stringify(mensaje));
-	//req_socket.send(mensaje);
-	
-    return res.json({codigo: 200, mensaje: 'request sended', responded: `Responded by: ${SERVICE_ID}`});
+	dealer.send(['','','', JSON.stringify(mensaje), 'peticion']);
+	while(isDone){
+		await sleep(300);
+	}
+    return res.json({code: 200, response: `${resp}`, responded: `Responded by: ${SERVICE_ID}`});
   });
   
   service.get('/frontEnd/prove', (req, res, next) => {
